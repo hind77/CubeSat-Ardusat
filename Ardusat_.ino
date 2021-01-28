1438bab0977e746e1fa208fca7cd5ab29e393d48
hind77 <hind.boukhairat@gmail.com>
2021-01-28 16:43:43 +0100

    ardusat2

--- Ardusat_.ino
+++ Ardusat_.ino
@@ -7,13 +7,15 @@
 #include <dht.h>
 #include <Wire.h>  
 
+#define DHTPIN 8     // what pin we're connected to
+#define DHTTYPE DHT22   // DHT 22  (AM2302)
+#define co2Zero     55                        //calibrated CO2 0 level
 
 DS1302:  RST pin    -> Arduino Digital 2
           DATA pin   -> Arduino Digital 3
           CLK pin  -> Arduino Digital 5
 DS1302 rtc(2, 3, 5);
 int greenPin = 8;
-//dht DHT;
 String UVs = "0"; 
 int UVn;
 const int mq135Pin = 0;
@@ -26,10 +28,7 @@ float m = -0.318; //Slope
 float b = 1.133; //Y-Intercept
 int pinCS = 10; // Pin 53 on Arduino mega
 File myFile;
-#define co2Zero     55                        //calibrated CO2 0 level
 MQ135 gasSensor = MQ135(mq135Pin);  // Initialise l'objet MQ135 sur le Pin spécifié
-#define DHTPIN 8     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
 DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 int chk;
 float hum;  //Stores humidity value
@@ -38,35 +37,35 @@ float temp; //Stores temperature value
 
 void setup()
 {
-  Serial.begin(9600);  // Used to type in characters  
-  float rzero = gasSensor.getRZero();
-  //Serial.print("R0: ");
-  //Serial.println(rzero);  // Valeur à reporter ligne 27 du fichier mq135.h après 48h de préchauffage
+    Serial.begin(9600);  // Used to type in characters  
+    float rzero = gasSensor.getRZero();
+    //Serial.print("R0: ");
+    //Serial.println(rzero);  // Valeur à reporter ligne 27 du fichier mq135.h après 48h de préchauffage
+      
+    // Set the clock to run-mode, and disable the write protection
+    rtc.halt(false);
+    rtc.writeProtect(false);
     
-  // Set the clock to run-mode, and disable the write protection
-  rtc.halt(false);
-  rtc.writeProtect(false);
+    // Setup LCD to 16x2 characters
+    // lcd.begin(16, 2);
   
-  // Setup LCD to 16x2 characters
-  // lcd.begin(16, 2);
-
-
-  // The following lines can be commented out to use the values already stored in the DS1302
-  rtc.setDOW(WEDNESDAY);        // Set Day-of-Week to FRIDAY
-  rtc.setTime(17, 3, 40);     // Set the time to 12:00:00 (24hr format)
-  rtc.setDate(14, 2, 2018);   // Set the date to August 6th, 2010
-  pinMode(gas_sensor, INPUT);
-  pinMode(pinCS, OUTPUT);
   
-  // SD Card Initialization
-  if (SD.begin())
-  {
-    Serial.println("SD card is ready to use.");
-  } else
-  {
-    Serial.println("SD card initialization failed");
-    return;
-  }
+    // The following lines can be commented out to use the values already stored in the DS1302
+    rtc.setDOW(WEDNESDAY);        // Set Day-of-Week to FRIDAY
+    rtc.setTime(17, 3, 40);     // Set the time to 12:00:00 (24hr format)
+    rtc.setDate(14, 2, 2018);   // Set the date to August 6th, 2010
+    pinMode(gas_sensor, INPUT);
+    pinMode(pinCS, OUTPUT);
+    
+    // SD Card Initialization
+    if (SD.begin())
+    {
+      Serial.println("SD card is ready to use.");
+    } else
+    {
+      Serial.println("SD card initialization failed");
+      return;
+    }
   
 }
 
@@ -74,103 +73,103 @@ void setup()
 void loop()
 {
 
- UVs = readSensor();
- UVn =val();
- 
-int co2now[10];                               //int array for co2 readings
-int co2raw = 0;                               //int for raw value of co2
-int co2comp = 0;                              //int for compensated co2 
-int co2ppm = 0;                               //int for calculated ppm
-int zzz = 0;                                  //int for averaging
-int grafX = 0;                                //int for x value of graph
-
-
+   UVs = readSensor();
+   UVn =val();
+   
+  int co2now[10];                               //int array for co2 readings
+  int co2raw = 0;                               //int for raw value of co2
+  int co2comp = 0;                              //int for compensated co2 
+  int co2ppm = 0;                               //int for calculated ppm
+  int overgaging = 0;                           //int for averaging
+  int grafX = 0;                                //int for x value of graph
   
-for (int x = 0;x<10;x++){                   //samplpe co2 10x over 2 seconds
-    co2now[x]=analogRead(A1);
-    delay(200);
-  }
-
-for (int x = 0;x<10;x++){                     //add samples together
-    zzz=zzz + co2now[x];
+  
     
-  }
-  co2raw = zzz/10;                            //divide samples by 10
-  co2comp = co2raw - co2Zero;                 //get compensated value
-  co2ppm = map(co2comp,0,1023,400,5000);      //map value for atmospheric levels
-  int ck = DHT.read11(greenPin);
-  float ppm2 = gasSensor.getPPM();
-
-  // methan gas sensor
-
-  int sensorValue;
-  int meth;
-  sensor_volt; //Define variable for sensor voltage
-  float RS_gas; //Define variable for sensor resistance
-  float ratio; //Define variable for ratio
-  float sensorValue = analogRead(gas_sensor); //Read analog values of sensor
-  sensor_volt = sensorValue * (5.0 / 1023.0); //Convert analog values to voltage
-  RS_gas = ((5.0 * 10.0) / sensor_volt) - 10.0; //Get value of RS in a gas
-  ratio = RS_gas / R0;   // Get ratio RS_gas/RS_air
-
-  double ppm_log = (log10(ratio) - b) / m; //Get ppm value in linear scale according to the the ratio value
-  double ppm = pow(10, ppm_log); //Convert ppm value to log scale
-  double ppb = 1000*ppm;
-
-
-  sensorValue = analogRead(0);       // read analog input pin 0
-  meth = map(sensorValue, 0, 1023, 0, 5000);
-
-   hum = dht.readHumidity();
-    temp= dht.readTemperature();  
-  // file work
-  Serial.print(rtc.getDOWStr(FORMAT_SHORT));
-  Serial.print(",");
-  Serial.print(rtc.getDateStr());
-  Serial.print(",");
-  Serial.print(rtc.getTimeStr());
-  Serial.print(",");
-  Serial.print(hum);
-  Serial.print(",");
-  Serial.print(temp);
-  Serial.print(",");
-  Serial.print(meth);
-  Serial.print(",");
-  Serial.print(co2ppm);
-  Serial.print(",");
-  Serial.print(UVn);
-  Serial.print(",");
-  Serial.println(UVs);
-
-
-  myFile = SD.open("test.txt", FILE_WRITE);
-  if (myFile) {   
-  myFile.print(rtc.getDOWStr(FORMAT_SHORT));
-  myFile.print(",");
-  myFile.print(rtc.getDateStr());
-  myFile.print(",");
-  myFile.print(rtc.getTimeStr());
-  myFile.print(",");
-  myFile.print(hum);
-  myFile.print(",");
-  myFile.print(temp);
-  myFile.print(",");
-  myFile.print(meth);
-  myFile.print(",");
-  myFile.print(co2ppm);
-  myFile.print(",");
-  myFile.print(UVn);
-  myFile.print(",");
-  myFile.println(UVs);
-
-
-    myFile.close(); // close the file
-  }
-  // if the file didn't open, print an error:
-  else {
-    Serial.println("error opening test.txt");
-  }
-  delay(3000);
+  for (int x = 0;x<10;x++){                   //samplpe co2 10x over 2 seconds
+      co2now[x]=analogRead(A1);
+      delay(200);
+    }
+  
+  for (int x = 0;x<10;x++){                     //add samples together
+      overgaging= overgaging+ co2now[x];
+      
+    }
+    co2raw = zzz/10;                            //divide samples by 10
+    co2comp = co2raw - co2Zero;                 //get compensated value
+    co2ppm = map(co2comp,0,1023,400,5000);      //map value for atmospheric levels
+    int ck = DHT.read11(greenPin);
+    float ppm2 = gasSensor.getPPM();
+  
+    // methan gas sensor
+  
+    int sensorValue;
+    int meth;
+    sensor_volt; //Define variable for sensor voltage
+    float RS_gas; //Define variable for sensor resistance
+    float ratio; //Define variable for ratio
+    float sensorValue = analogRead(gas_sensor); //Read analog values of sensor
+    sensor_volt = sensorValue * (5.0 / 1023.0); //Convert analog values to voltage
+    RS_gas = ((5.0 * 10.0) / sensor_volt) - 10.0; //Get value of RS in a gas
+    ratio = RS_gas / R0;   // Get ratio RS_gas/RS_air
+  
+    double ppm_log = (log10(ratio) - b) / m; //Get ppm value in linear scale according to the the ratio value
+    double ppm = pow(10, ppm_log); //Convert ppm value to log scale
+    double ppb = 1000*ppm;
+  
+  
+    sensorValue = analogRead(0);       // read analog input pin 0
+    meth = map(sensorValue, 0, 1023, 0, 5000);
+  
+     hum = dht.readHumidity();
+      temp= dht.readTemperature();  
+    // file work
+    Serial.print(rtc.getDOWStr(FORMAT_SHORT));
+    Serial.print(",");
+    Serial.print(rtc.getDateStr());
+    Serial.print(",");
+    Serial.print(rtc.getTimeStr());
+    Serial.print(",");
+    Serial.print(hum);
+    Serial.print(",");
+    Serial.print(temp);
+    Serial.print(",");
+    Serial.print(meth);
+    Serial.print(",");
+    Serial.print(co2ppm);
+    Serial.print(",");
+    Serial.print(UVn);
+    Serial.print(",");
+    Serial.println(UVs);
+  
+  
+    myFile = SD.open("test.txt", FILE_WRITE);
+    if (myFile) {   
+    myFile.print(rtc.getDOWStr(FORMAT_SHORT));
+    myFile.print(",");
+    myFile.print(rtc.getDateStr());
+    myFile.print(",");
+    myFile.print(rtc.getTimeStr());
+    myFile.print(",");
+    myFile.print(hum);
+    myFile.print(",");
+    myFile.print(temp);
+    myFile.print(",");
+    myFile.print(meth);
+    myFile.print(",");
+    myFile.print(co2ppm);
+    myFile.print(",");
+    myFile.print(UVn);
+    myFile.print(",");
+    myFile.println(UVs);
+  
+  
+      myFile.close(); // close the file
+    }
+    // if the file didn't open, print an error:
+    else {
+      Serial.println("error opening test.txt");
+    }
+    delay(3000);
  
 }
 
